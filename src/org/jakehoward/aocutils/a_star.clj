3e8644dfd872ed2e981b1fc0b08382cad20d314f
Jake Howard <jake@jakehowrd.org>
2023-11-23 22:12:18 +0000

    More perf exploration, add a-star algo

---
+++ src/org/jakehoward/aocutils/a_star.clj
@@ -0,0 +1,112 @@
+{:nextjournal.clerk/visibility {:code :hide :result :hide}}
+(ns org.jakehoward.aocutils.a-star
+  (:require [clojure.string :as str]))
+
+;; # A-Star
+
+{:nextjournal.clerk/visibility {:code :show :result :hide}}
+(defn min-by [f coll]
+  (when (seq coll)
+    (reduce (fn [min other]
+              (if (> (f min) (f other))
+                other
+                min))
+            coll)))
+
+(defn neighbours [size yx]
+  (let [deltas [[-1 0] [1 0] [0 -1] [0 1]]]
+    (->> deltas
+         (map #(vec (map + yx %)))
+         (filter (fn [new-yx] (every? #(< -1 % size) new-yx))))))
+
+(defn joy-heuristic [step-cost-est size y x]
+  (* step-cost-est
+     (- (+ size size) y x 2)))
+
+;; estimate-cost => h(...) in the literature (presumably "heuristic")
+(def estimate-cost joy-heuristic)
+
+;; path-cost => g(...) in the literature (presumably "god, it's annoying when ppl use symbols")
+(defn path-cost [node-cost cheapest-nbr cost-to-move]
+  (+ node-cost cost-to-move (* cost-to-move (count (:xys cheapest-nbr [])))
+     (or (:cost cheapest-nbr) 0)))
+
+;; total-cost => f(...) in the literature
+(defn total-cost [newcost step-cost-est size [y x]]
+  (+ newcost
+     (estimate-cost step-cost-est size y x)))
+
+(defn a-star [start-yx step-est cell-costs cost-to-move]
+  (let [size (count cell-costs)]
+    (loop [steps 0
+           routes (vec (repeat size (vec (repeat size nil))))
+           work-todo (sorted-set [0 start-yx])]
+      (if (empty? work-todo)
+        [(peek (peek routes)) :steps steps]
+        (let [[_ yx :as work-item] (first work-todo)
+              rest-work-todo (disj work-todo work-item)
+              nbr-yxs (neighbours size yx)
+              cheapest-nbr (min-by :cost (keep #(get-in routes %) nbr-yxs))
+              newcost (path-cost (get-in cell-costs yx) cheapest-nbr cost-to-move)
+              oldcost (:cost (get-in routes yx))]
+          (if (and oldcost (>= newcost oldcost))
+            (recur (inc steps) routes rest-work-todo)
+            (recur (inc steps)
+                   (assoc-in routes yx {:cost newcost :yxs (conj (:yxs cheapest-nbr []) yx)})
+                   (into rest-work-todo
+                         (map (fn [w] [(total-cost newcost step-est size w) w]) nbr-yxs)))))))))
+
+{:nextjournal.clerk/visibility {:code :hide :result :hide}}
+(defn lpad [n char s]
+  (str (str/join (repeat (- n (count (str s))) char)) s))
+
+(defn rpad [n char s]
+  (str s (str/join (repeat (- n (count (str s))) char))))
+
+(defn visualise-route [world route]
+  (let [route-set (set route)
+        max-len (apply max (mapcat (fn [row] (map (comp count str) row)) world))]
+    (->> (for [y (range (count world)) x (range (count world))]
+           (let [str-cell (get-in world [y x])]
+             (if (route-set [y x])
+               (lpad (inc max-len) " " (str "*" str-cell))
+               (lpad (inc max-len) " " (str "" str-cell)))))
+         (partition (count world))
+         (map #(str/join " " %))
+         (str/join "\n"))))
+
+(defn print-viz-a-star [start-yx step-est world cost-to-move]
+  (let [[result _ steps] (a-star start-yx step-est world cost-to-move)]
+    (println "Steps:" steps)
+    (println "Cost:" (:cost result))
+    (println
+     (visualise-route world (:yxs result)))))
+
+(defn viz-a-star [start-yx step-est world cost-to-move]
+  (let [[result _ steps] (a-star start-yx step-est world cost-to-move)]
+    (println "Steps:" steps)
+    (println "Cost:" (:cost result))
+    (visualise-route world (:yxs result))))
+
+(comment
+  (let [world [[1 1 1 1 1]
+               [999 999 999 999 1]
+               [1 1 1 1 1]
+               [1 999 999 999 999]
+               [1 1 1 1 1]]]
+    (print-viz-a-star [0 0] 1 world 1))
+
+  (let [world [[1 1 1 1 1]
+               [999 999 1 999 999]
+               [1 1 1 1 1]
+               [999 999 1 999 999]
+               [1 1 1 1 1]]]
+    (print-viz-a-star [0 0] 1 world 1)))
+
+{:nextjournal.clerk/visibility {:code :show :result :show}}
+(let [world [[1,,,1,,,1,,,1,,,1]
+             [999 999 999 999 1]
+             [1,,,1,,,1,,,1,,,1]
+             [1,,,999 999 999 999]
+             [1,,,1,,,1,,,1,,,1]]]
+  (viz-a-star [0 0] 1 world 1))
--- src/org/jakehoward/aocutils/performance.clj
+++ src/org/jakehoward/aocutils/performance.clj
@@ -27,11 +27,11 @@
 {:nextjournal.clerk/visibility {:code :show :result :hide}}
 (comment
   (quick-bench
-      (loop [n 0
-             m {}]
-        (if (< n 10000)
-          (recur (inc n) (assoc m :n n))
-          m))))
+   (loop [n 0
+          m {}]
+     (if (< n 10000)
+       (recur (inc n) (assoc m :n n))
+       m))))
 
 ;; - => 233 µs to 400 µs
 ;; - => verdict: baseline!
@@ -40,11 +40,11 @@
 {:nextjournal.clerk/visibility {:code :show :result :hide}}
 (comment
   (quick-bench
-      (loop [n 0
-             m (transient {})]
-        (if (< n 10000)
-          (recur (inc n) (assoc! m :n n))
-          (persistent! m)))))
+   (loop [n 0
+          m (transient {})]
+     (if (< n 10000)
+       (recur (inc n) (assoc! m :n n))
+       (persistent! m)))))
 
 ;; - => 80 µs to 122 µs
 ;; - => verdict: good speedup
@@ -52,15 +52,14 @@
 ;; ### 3. Using a Java HashMap
 (comment
   (quick-bench
-      (loop [n 0
-             m (java.util.HashMap.)]
-        (if (< n 10000)
-          (recur (inc n) (do (.put m :n n) m))
-          m))))
+   (loop [n 0
+          m (java.util.HashMap.)]
+     (if (< n 10000)
+       (recur (inc n) (do (.put m :n n) m))
+       m))))
 
 ;; - => 61 µs to 105 µs
 ;; - => verdict: annoying enough that put doesn't return it's not worth it over transient
-
 
 ;; ## Iterating over sequences
 ;; Some problems are well modelled by sequences of numbers. Let's
@@ -68,34 +67,124 @@
 
 ;; ### 1. Unrealised lazy-seq
 (comment
-  (let [my-seq (range 10000)]
+  (let [my-seq (map identity (range 10000))]
     (quick-bench
-        (doall (filter even? my-seq)))))
-;; => 214 µs to 257 µs
+     (doall (filter even? my-seq)))))
+;; => 178 µs to 225 µs
 
 ;; ### 2. Realised lazy-seq
 (comment
-  (let [my-seq (doall (range 10000))]
+  (let [my-seq (doall (map identity (range 10000)))]
     (quick-bench
-        (doall (filter even? my-seq)))))
-;; - => 219 µs to 296 µs
+     (doall (filter even? my-seq)))))
+;; - => 178 µs to 219 µs
 ;; - => did I do the test right?
 
 ;; ### 3. A vector, normal filter
 (comment
   (let [my-seq (vec (range 10000))]
     (quick-bench
-        (doall (filter even? my-seq)))))
+     (doall (filter even? my-seq)))))
 ;; - => 217 µs to 223 µs
-
 
 ;; ### 4. A vector, filterv
 (comment
   (let [my-seq (vec (range 10000))]
     (quick-bench
-        (filterv even? my-seq))))
+     (filterv even? my-seq))))
 ;; - =>  157 µs to 160 µs
 
+;; ### 5. A vector, normal map
+(comment
+  (let [my-seq (vec (range 10000))]
+    (quick-bench
+     (doall (map #(* % %) my-seq)))))
+;; - => 262 µs to 564 µs
 
-;; - =>  µs to  µs
+;; ### 6. A vector, mapv
+(comment
+  (let [my-seq (vec (range 10000))]
+    (quick-bench
+     (mapv #(* % %) my-seq))))
+;; - =>  182 µs to 190 µs
 
+;; ### 6.1. A Java array
+(comment
+  (let [my-seq (int-array (range 10000))]
+    (quick-bench
+     (doall (map inc my-seq)))))
+;; - =>  822 µs to 1,116 µs
+
+{:nextjournal.clerk/visibility {:code :show :result :show}}
+;; ### 6.2. A Java array (bash in place)
+(let [xs (int-array (range 5))]
+  (dotimes [idx (alength xs)]
+    (aset xs idx (inc (aget xs idx))))
+  (seq xs))
+
+(comment
+  (let [my-seq ^"[I" (int-array (range 10000))]
+    (quick-bench
+     (dotimes [idx (alength my-seq)]
+       (aset-int my-seq idx (inc (aget my-seq idx)))))))
+;; - =>  314 µs to 459 µs
+;; - => am I doing something wrong? Expected this to be fast... => yes! use aset not aset-int
+
+(comment
+  (do
+    (set! *warn-on-reflection* true)
+    (let [my-seq (long-array (range 10000))
+          len    (alength my-seq)]
+      (quick-bench
+       (loop [idx 0]
+         (when (< idx len)
+           (do
+             (aset my-seq idx (unchecked-add-int 1 (aget my-seq idx)))
+             (recur (unchecked-add-int idx 1)))))))
+    (set! *warn-on-reflection* false)))
+;; - => 7 µs to 7.4 µs
+;; - => Finally! So, unless you're measuring and being careful, not faster
+;; - => If you are being careful sooooo much faster!
+
+{:nextjournal.clerk/visibility {:code :show :result :hide}}
+;; ### 7. A vector, mapv and filterv (to compare to transduce)
+(comment
+  (let [my-seq (vec (range 10000))]
+    (quick-bench
+     (->> my-seq
+          (mapv #(* % %))
+          (filterv even?)))))
+;; - =>  335 µs to 353 µs
+
+;; ## Transducers
+;; ### 8. A vector, map and filter (transducer)
+(comment
+  (let [my-seq (vec (range 10000))
+        xf     (comp
+                (map #(* % %))
+                (filter even?))]
+    (quick-bench
+     (into [] xf my-seq))))
+;; - =>  282 µs to 306 µs
+
+;; ### 9. A vector, map and filter (swap order) (transducer)
+(comment
+  (let [my-seq (vec (range 10000))
+        xf     (comp
+                (filter even?)
+                (map #(* % %)))]
+    (quick-bench
+     (into [] xf my-seq))))
+;; - =>  239 µs to 259 µs
+
+;; ## Reduced
+;; ### 10. short circuiting a reduce
+(comment
+  (let [my-seq (vec (range 10000))
+        f      (fn [acc item] (if (< item 1000) (+ acc item) (reduced :big)))]
+    (quick-bench
+     (reduce f my-seq)))) ;; (returns :big)
+;; - =>  15 µs to 21 µs
+
+;; ## TODO
+;; - Investigate `(set! *warn-on-reflection* true)`
